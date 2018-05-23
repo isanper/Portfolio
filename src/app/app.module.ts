13df34144c814096e64a0419bfc36f10042a4991
Ismael Santana Perez <isma.sps2@gmail.com>
2018-05-23 09:44:22 +0100

    Cabecera, menu, social media y timeline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CabeceraComponent,
+    TimelineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
