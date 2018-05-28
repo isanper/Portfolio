91d099a51e115a1f61e9878e28201971ea115ef3
Ismael Santana Perez <isma.sps2@gmail.com>
2018-05-28 11:56:22 +0100

    Añadidos efectos css a botones y timeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent {
 
   public ngOnInit(){
+    AOS.init();
   }
   
 }
--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -14,5 +14,21 @@ export class CabeceraComponent implements OnInit {
 
   showmenu = false;
   showsocial = false;
+  numClicks = 0;
+  elementSelected: string = "";
 
+  showDesc(element: string) {
+    this.numClicks++;
+    if(this.numClicks === 1){
+      this.elementSelected = element;
+    }else if(this.numClicks === 2 && element == this.elementSelected){
+      console.log(this.elementSelected)
+      console.log(element)
+      console.log('Segundo click')
+      this.numClicks = 0;
+    }else{
+      this.numClicks = 0;
+      this.elementSelected = "";
+    }
+  }
 }
