d6603be8122abc48a168914d6f724db813c2a07a
Ismael Santana Perez <isma.sps2@gmail.com>
2018-05-31 16:15:09 +0100

    nuevo componente about y proyecto v2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
@@ -8,14 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { TimelineComponent } from './timeline/timeline.component';
-
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CabeceraComponent,
-    TimelineComponent
+    TimelineComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
